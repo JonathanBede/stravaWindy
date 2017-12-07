192e063d0b5f43c47bac9334783ca760b0bf47b9
Jonathan Burton <jonathan.burton@bedegaming.com>
2017-12-07 14:48:24 +0000

    weather call added

--- src/Weather.js
+++ src/Weather.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import Axios from 'axios';
 
 class Weather extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            windDirection: []
+        };
+      }
 
 componentDidMount() {
-  Axios.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/1f0ce7cb865b83d6ba17a661fcf9f49f/37.8267,-122.4233')
-        .then(function (response) {
-            console.log(response);
+  Axios.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/1f0ce7cb865b83d6ba17a661fcf9f49f/37.8267,-122.4233?time=[2017]-[09]-[13]T[00]:[00]:[00]&exclude=[hourly,minutely,daily,flags,alerts]')
+        .then((response) => {
+            this.setState({windDirection: response.data.currently.windBearing})
         });
 };
 
-  render() {
+render() {
     return (
-      <div>Weather</div>
+      <div>{this.state.windDirection}</div>
     );
   }
 }
