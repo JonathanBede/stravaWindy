c11306394cde849d282af4b81991cb28a0f033ef
jakescutt <jake.scutt@bedegaming.com>
2017-12-07 13:22:48 +0000

    fetch and render segment data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Strava from './Strava';
 import Weather from './Weather';
+import 'normalize.css';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Strava</h1>
+        <header><h1>Strava Weather</h1></header>
         <Strava></Strava>
         <Weather></Weather>
       </div>
--- src/Strava.js
+++ src/Strava.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 
-import './App.css';
+const testID = 12064511
+
+const urlForSegment = segmentID => `https://www.strava.com/api/v3/segments/${segmentID}?access_token=ea0e82c6e8cc3d19a57f22823f175b52062ce456`
+const urlForLeaderboard = segmentID => `https://www.strava.com/api/v3/segments/${segmentID}/leaderboard?access_token=ea0e82c6e8cc3d19a57f22823f175b52062ce456`
 
 class Strava extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      segmentData: [],
+      leaderData: {
+        entries: []
+      }
+    }
+  }
 
-componentDidMount() {
-  Axios.get('https://www.strava.com/api/v3/segments/12064511/leaderboard?access_token=ea0e82c6e8cc3d19a57f22823f175b52062ce456')
-
-    .then(function (response) {
-        console.log(response);
+  getSegment() {
+    Axios.get(urlForSegment(testID))
+      .then((response) => {
+        this.setState({segmentData: response.data})
+      })
+    .catch((error)=>{
+        console.log(error);
     });
-};
+  };
+
+  getLeaderboard() {
+    Axios.get(urlForLeaderboard(testID))
+    .then((response) => {
+      this.setState({leaderData: response.data})
+    })
+  .catch((error)=>{
+      console.log(error);
+  });
+  console.log(this.state.leaderData);
+  };
+
+  componentDidMount() {
+    this.getLeaderboard();
+    this.getSegment();
+  }
 
 
   render() {
     return (
-      <div>Strava</div>
+      <div className="search-results-continer">
+        <div className="search-segment-name">{this.state.segmentData.name}</div>
+        <div className="search-results">
+          <ul>
+            {this.state.leaderData.entries.map(item =>
+              <li key={item.athlete_name}>{item.athlete_name} - {item.elapsed_time}</li>
+            )}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
