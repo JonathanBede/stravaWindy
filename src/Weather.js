83960a0e3a21fa233ae0f9a7f57195e82d77e02a
Jonathan Burton <jonathan.burton@bedegaming.com>
2017-12-07 13:37:12 +0000

    weather change

--- src/Weather.js
+++ src/Weather.js
@@ -4,13 +4,11 @@ import Axios from 'axios';
 class Weather extends Component {
 
 componentDidMount() {
-  Axios.get('https://api.darksky.net/forecast/1f0ce7cb865b83d6ba17a661fcf9f49f/37.8267,-122.4233')
-
-    .then(function (response) {
-        console.log(response);
-    });
+  Axios.get('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/1f0ce7cb865b83d6ba17a661fcf9f49f/37.8267,-122.4233')
+        .then(function (response) {
+            console.log(response);
+        });
 };
-
 
   render() {
     return (
